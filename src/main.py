014de75a6a1aed9a2e85e71036992295f545c73d
Andrew Skow <skovorodnikov11@gmail.com>
2023-10-13 16:53:55 +0300

    корректный вывод всей инфы о транзакциях, выполнено всё по тз, далее - добавить тесты.

--- src/main.py
+++ src/main.py
@@ -21,5 +21,12 @@ for transaction in sorted_five_ex_transactions:
     else:
         print("... -> " + convert_bank_numbers(transaction['to']))
 
+    print(transaction['operationAmount']['amount'],
+          transaction['operationAmount']['currency']['name'])
 
     print()
+
+# Пример вывода для одной операции:
+# 14.10.2018 Перевод организации
+# Visa Platinum 7000 79** **** 6361 -> Счет **9638
+# 82771.72 руб.
